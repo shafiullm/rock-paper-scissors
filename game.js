475c633ff594689a3f2d1b96e11994e534d4a520
Shafiul Alam <shafiullm@gmail.com>
2023-08-23 23:06:36 +0600

    Create UI of RPS

--- game.js
+++ game.js
@@ -1,67 +1,137 @@
-function getComputerChoice(){
-    choices = ['rock','paper','scissor'];
-    return choices[Math.floor(Math.random()*3)];
+var introContainer = document.querySelector(".intro-container");
+var mainContent = document.querySelector(".main-content");
+
+setTimeout(removeMainIntro, 6000);
+setTimeout(addMainContent, 6000);
+
+function removeMainIntro(){
+    introContainer.classList.remove('.intro-container');
 }
 
-function playRound(playerChoice,computerChoice){
-    playerChoice = playerChoice.replace(/\s+/g, '').toLowerCase();
-    output = '';
+function addMainContent() {
+    var htmlContent = `
+      <div class="headerText">
+        <h1>Select to Win-Tie-Lose</h1>
+      </div>
+      <div style="display: flex; flex-direction: row; justify-content: space-evenly; align-items: center; font-size: 15px; color: white;" class="score">
+        <div>User score: <b><span class= "scoreDisplayUser">0</span></b></div>
+        <div>CPU score: <b><span class= "scoreDisplayCPU">0</span></b></div>
+      </div>
+      <div class="imageContainer">
+        <div data-text="Rock" value="rock" class="imageChildContainer">
+          <img src="assets/rockPNG.png">
+        </div>
+        <div data-text="Paper" value="paper" class="imageChildContainer">
+          <img src="assets/paperPNG.png">
+        </div>
+        <div data-text="Scissor" value="scissor"  class="imageChildContainer">
+          <img src="assets/scissorPNG.png">
+        </div>
+      </div>
+    `;
 
-    if(playerChoice === computerChoice){
-        output+= 'Tied!';
-    }
+    mainContent.innerHTML = htmlContent;
 
-    else if(playerChoice === 'rock'){
-        if(computerChoice === 'paper'){
-            output+=  'You Lost!';
-        }
-        else{
-            output+=  'You win!';
-        }
-    }
+    var imageDivs = document.querySelectorAll(".imageChildContainer");
+    var CPUScore = 0;
+    var userScore = 0;
 
-    else if(playerChoice === 'paper'){
-        if(computerChoice === 'scissor'){
-            output+=  'You Lost!';
-        }
-        else{
-            output+=  'You win!';
-        }
-    }
+    imageDivs.forEach(function(div) {
+    div.addEventListener("click", function() {
+    var selectedValue = this.getAttribute("value");
+    var computerValue = getComputerChoice();
+    
 
-    else if(playerChoice === 'scissor'){
-        if(computerChoice === 'rock'){
-            output+=  'You Lost!';
-        }
-        else{
-            output+=  'You win!';
-        }
+    function getComputerChoice(){
+        choices = ['rock','paper','scissor'];
+        return choices[Math.floor(Math.random()*3)];
     }
     
-    alert("You: " + playerChoice+"\nComputer: " + computerChoice+"\n"+output);
-    return output;
-}
+    function playRound(playerChoice,computerChoice){
+        output = '';
+        var audioRes;
 
-function game(){
-    var userScore = 0;
-    var CPUScore = 0;
-    alert("You are going to play 5 rounds of rock, paper and scissors!");
-    for(i=0; i<5; i++){
-        const output = playRound(prompt("Enter your choice (rock/paper/scissor): "), getComputerChoice());  
-        if (output == 'You win!'){
-            userScore++;
+        if(playerChoice === computerChoice){
+            output+= 'Tie';
+            audioRes = 'tieAudio';
         }
-        else if (output == 'You Lost!'){
-            CPUScore++;
+    
+        else if(playerChoice === 'rock'){
+            if(computerChoice === 'paper'){
+                output+=  'Lose';
+                CPUScore+=1;
+                audioRes = 'loseAudio';
+            }
+            else{
+                output+=  'Win';
+                userScore+=1;
+                audioRes = 'winAudio';
+            }
         }
-    }
+    
+        else if(playerChoice === 'paper'){
+            if(computerChoice === 'scissor'){
+                output+=  'Lose';
+                CPUScore+=1;
+                audioRes = 'loseAudio';
+            }
+            else{
+                output+=  'Win';
+                userScore+=1;
+                audioRes = 'winAudio';
+            }
+        }
+    
+        else if(playerChoice === 'scissor'){
+            if(computerChoice === 'rock'){
+                output+=  'Lose';
+                CPUScore+=1;
+                audioRes = 'loseAudio';
+            }
+            else{
+                output+=  'Win';
+                userScore+=1;
+                audioRes = 'winAudio';
+            }
+        }
 
-    if (userScore>CPUScore){
-        alert("You are the winner! User Score: "+userScore+", CPU Score: "+CPUScore);
-    }
-    else{
-        alert("You are the loser! User Score: "+userScore+", CPU Score: "+CPUScore);
-    }
-}
+        var scoreDisplayUser = document.querySelector('.scoreDisplayUser');
+        var scoreDisplayCPU = document.querySelector('.scoreDisplayCPU');
 
-game();
+        scoreDisplayUser.textContent = userScore;
+        scoreDisplayCPU.textContent = CPUScore;
+
+
+        var audio = document.querySelector(`.${audioRes}`);
+
+        audio.volume = 0.2;
+        audio.currentTime = 0;
+        audio.play();
+
+        return output;
+    }
+    
+
+
+    
+      selectionText = document.createElement('h2');
+      selectionText.classList.add('resultText');
+      selectionText.textContent = `You: ${selectedValue.charAt(0).toUpperCase() + selectedValue.slice(1)}, CPU: ${computerValue.charAt(0).toUpperCase() + computerValue.slice(1)}, Result: ${playRound(selectedValue, computerValue)}`;
+      mainContent.appendChild(selectionText);
+
+    });
+    
+  });
+  }
+
+
+
+  
+
+
+
+
+
+
+
+
